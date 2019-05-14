9681ec0bfacbbdd6b2695098fc66a751ed61512d
boffman <bkhoffman@gmail.com>
2019-05-14 15:03:32 -0500

    added friends arr/html routes/Home page structure

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+//setup empty array
+const friends = [
+  {
+    "name": "Brad",
+    "photo": "https://images.app.goo.gl/kdYVVc1KG6E49hW68",
+    "scores": [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  }
+];
+
+module.exports = friends;
