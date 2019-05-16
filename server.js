28d16eaa4d29f1961e9b20aa57c3d2f41848f4e6
boffman <bkhoffman@gmail.com>
2019-05-16 11:44:29 -0500

    added api and repo links to home/setup friend api route

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// require("./app/routing/apiRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function(){
