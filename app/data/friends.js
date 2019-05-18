b852d66c1cff9e64cb2b7022a90130bcfbe7a2e0
boffman <bkhoffman@gmail.com>
2019-05-18 15:04:13 -0500

    Added a Procfile

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,87 @@
 //setup array of friends to compare
 const friends = [
   {
-    "name": "Brad",
-    "photo": "https://images.app.goo.gl/kdYVVc1KG6E49hW68",
+    "name": "Andrew Clark",
+    "photo": "https://www.imdb.com/title/tt0088847/mediaviewer/rm3206751744?ft0=name&fv0=nm0000389&ft1=image_type&fv1=still_frame",
     "scores": [
+      "3",
       "5",
-      "1",
       "4",
+      "5",
+      "2",
       "4",
+      "3",
+      "5",
+      "3",
+      "5"
+    ]
+  },
+  {
+    "name": "Brian Johnson",
+    "photo": "https://www.imdb.com/title/tt0088847/mediaviewer/rm3214875904?ft0=name&fv0=nm0001309&ft1=image_type&fv1=still_frame",
+    "scores": [
+      "2",
+      "5",
+      "2",
       "5",
       "1",
       "2",
+      "2",
+      "3",
+      "5",
+      "1"
+    ]
+  },
+  {
+    "name": "John Bender",
+    "photo": "https://www.imdb.com/title/tt0088847/mediaviewer/rm956498944?ft0=name&fv0=nm0000555&ft1=image_type&fv1=still_frame",
+    "scores": [
+      "5",
+      "3",
+      "4",
+      "1",
+      "2",
+      "5",
+      "5",
+      "2",
+      "1",
+      "2"
+    ]
+  },
+
+  {
+    "name": "Claire Standish",
+    "photo": "https://www.imdb.com/title/tt0088847/mediaviewer/rm3058145280?ft0=name&fv0=nm0000208&ft1=image_type&fv1=still_frame",
+    "scores": [
+      "4",
+      "2",
+      "2",
+      "3",
+      "3",
+      "2",
+      "2",
+      "2",
+      "4",
+      "5"
+    ]
+  },
+  {
+    "name": "Allison Reynolds",
+    "photo": "https://www.imdb.com/title/tt0088847/mediaviewer/rm3384019712?ft0=name&fv0=nm0000639&ft1=image_type&fv1=still_frame",
+    "scores": [
+      "5",
+      "1",
+      "3",
+      "2",
       "5",
       "4",
+      "2",
+      "1",
+      "2",
       "1"
     ]
   }
+
 ];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,5 +6,30 @@ module.exports = function(app) {
   });
   app.post("/api/friends", function(req, res){
     friendData.push(req.body);
+    //setup friend obj to populate and test against until lowest scoreDiff is reached and loop ends
+    let bestFriend = {
+      name: "",
+      photo: "",
+      scoreDiff: 1000
+    };
+    //scores for newly entered friend
+    let newFriendScores = req.body.scores;
+    //loop through friends arr and totalDiff var to add scores up
+    for(let i = 0; i < friendData.length; i++){
+      let totalDifference = 0;
+      //loop through Scores as we loop through friends arr
+      //parseInt the strings to numbers, subtract new Friends scores at each index and each friend in arr scores at same index 
+      for(let j = 0; j < friendData[i].scores[j]; j++){
+        totalDifference += Math.abs(parseInt(newFriendScores[j]) - parseInt(friendData[i].scores[j]));
+        //check Difference against scoreDiff to see which is lower and put in new bestFriend if lower
+        //update scoreDiff with the new "lowest" totalDiff to compare next cycle through
+        if(totalDifference < scoreDiff){
+          bestFriend.name = friendData[i].name;
+          bestFriend.photo = friendData[i].photo;
+          bestFriend.scoreDiff = totalDifference;
+        }
+      }
+    }
+    res.json(bestFriend);
   })
 }
